4264855f85ae0052ede3ba55e8ba9befc07dc2c3
BELLAMKONDAAMBICAKIRANMAYEE <ambica930@gmail.com>
2025-11-04 15:30:12 +0530

    first version commit

---
+++ ServletConfigExample/src/main/java/com/config/Servlet2.java
@@ -0,0 +1,20 @@
+package com.config;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class Servlet2 extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletConfig config = getServletConfig();
+		String x = config.getInitParameter("driver");
+		String y = config.getInitParameter("url");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>"+ x +"</h1>");
+		out.print("<h1>"+ y +"</h1>");
+	}
+}
